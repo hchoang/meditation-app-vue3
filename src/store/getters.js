8b13761a869f6c6d9989c334f09b71f2c2933933
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2023-08-13 14:21:26 +1000

    updated vuex getters

---
+++ src/store/getters.js
@@ -0,0 +1,17 @@
+export default {
+    timeSelected(state) {
+        return state.timeSelected
+    },
+    vibeSelected(state) {
+        return state.vibeSelected
+    },
+    step(state) {
+        return state.step
+    },
+    choices(state) {
+        return state.choices
+    },
+    isPlaying(state) {
+        return state.isPlaying
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -12,9 +12,48 @@ const store = createStore({
             vibeSelected: { value: 'birb' },
             step: 0,
             choices: [
+                { 
+                    id: 1,
+                    name: '10 minutes',
+                    imgSrc: require("@/assets/images/Bouton_10_minutes.png"),
+                    category: 'timer',
+                    value: 600,
+                },
+                { 
+                    id: 2,
+                    name: '20 minutes',
+                    imgSrc: require("@/assets/images/Bouton_20_minutes.png"),
+                    category: 'timer',
+                    value: 1200,
+                },
+                { 
+                    id: 3,
+                    name: '30 minutes',
+                    imgSrc: require("@/assets/images/Bouton_30_minutes.png"),
+                    category: 'timer',
+                    value: 1800,
+                },
                 {
-                    id: 1
-                }
+                    id: 4,
+                    name: 'In The Space',
+                    imgSrc: require("@/assets/images/Bouton_Space.png"),
+                    category: 'vibe',
+                    value: 'space',
+                },
+                {
+                    id: 5,
+                    name: 'On The Beach',
+                    imgSrc: require("@/assets/images/Bouton_Beach.png"),
+                    category: 'vibe',
+                    value: 'beach',
+                },
+                {
+                    id: 6,
+                    name: 'Under The Rain',
+                    imgSrc: require("@/assets/images/Bouton_Rain.png"),
+                    category: 'vibe',
+                    value: 'rain',
+                },
             ]
         }
     },
